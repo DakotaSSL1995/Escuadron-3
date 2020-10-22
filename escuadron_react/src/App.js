e07e1b8598d7979824915a36b87afe0b6b9246c4
Daniela Ponce <DanielaSPP1995@gmail.com>
2020-10-22 18:07:43 -0300

    Actualizacion

---
+++ escuadron_react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Faq from './Pages/Faq';
+import CapsulasC from './Pages/CapsulasC';
+
+function App() {
+  return (
+   <CapsulasC />
+  );
+}
+
+export default App;
---
+++ escuadron_react/src/Pages/Faq.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import NavBar from './Componentes/NavBar';
+import Footer from './Componentes/Footer';
+
+function Faq(){
+  return (
+    <>
+    <NavBar />
+    <button class="accordion">SECCIÓN 1</button>
+      <div class="panel">
+      <p>Prueba</p>
+        </div>
+    <button class="accordion">SECCIÓN 2</button>
+      <div class="panel">
+      <p>Prueba 2</p>
+      </div>
+
+    <button class="accordion">SECCIÓN 3</button>
+      <div class="panel">
+      <p>Prueba 3</p>
+      </div>
+      <Footer />
+</>
+  )
+}
+
+export default Faq;
